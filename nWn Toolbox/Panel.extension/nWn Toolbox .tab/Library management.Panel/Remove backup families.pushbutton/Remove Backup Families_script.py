781cd59c7cec853fc99c151a62bcbf04d1d1bab6
Wei <weinini007@gmail.com>
2019-05-25 20:46:10 +1000

    feat: add radius buttons to confirm user input

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Library management.Panel/Remove backup families.pushbutton/Remove Backup Families_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Library management.Panel/Remove backup families.pushbutton/Remove Backup Families_script.py
@@ -1,31 +1,89 @@
 # -*- coding: utf-8 -*-
-"""Load families from a directory.
+"""Remove backup files from a directory.
 
 Note:
 Please be as specific as possible selecting the folder,
-as this script that loads all families in subfolders."""
+as this script will remove all backup files in subfolders."""
 __title__ = 'Remove\nBackup\nFiles'
 __author__ = "nWn"
 
-# Import commom language runtime
+# Import clr
 import clr
 
-# from Autodesk.Revit.UI import *
-from Autodesk.Revit.DB import Transaction, TransactionGroup, IFamilyLoadOptions
-
-# Import Python modules
-import os
-import re
+# Import python modules
+import os, re, shutil
 
 # Import libraries to create a Windows form
 clr.AddReference("System.Windows.Forms")
 clr.AddReference("System.Drawing")
 
-from System.Windows.Forms import FolderBrowserDialog
-from System.Windows.Forms import DialogResult
+from System.Windows.Forms import Application, Form, StatusBar, FolderBrowserDialog
+from System.Windows.Forms import RadioButton, GroupBox, DialogResult, Button
+from System.Drawing import Size, Point
 
-# Store user specified path
+# Class to input user choice
+class IForm(Form):
+	def __init__(self):
+		self.Text = "RadioButton"
+		self.Size = Size(360, 240)
+		self.value = ""
+
+		gb = GroupBox()
+		gb.Text = "Remove files or Move to Another Location"
+		gb.Size = Size(300, 100)
+		gb.Location = Point(20, 20)
+		gb.Parent = self
+
+		remove = RadioButton()
+		remove.Text = "Remove"
+		remove.Parent = gb
+		remove.Location = Point(10, 30)
+		remove.CheckedChanged += self.OnChanged
+
+
+		backup = RadioButton()
+		backup.Text = "Move to Folder"
+		backup.Parent = gb
+		backup.Size = Size(300, 20)
+		backup.Location = Point(10, 60)
+		backup.CheckedChanged += self.OnChanged
+
+		# Create button
+		button = Button()
+		button.Parent = self
+		button.Text = "Ok"
+		button.Location = Point(130, 130)
+		# Register event
+		button.Click += self.ButtonClicked
+
+		self.statusbar = StatusBar()
+		self.statusbar.Parent = self
+
+		self.CenterToScreen()
+
+	def ButtonClicked(self, sender, event):
+		self.Close()
+
+	def OnChanged(self, sender, event):
+		if sender.Checked:
+			self.value = sender.Text
+    
+buttonForm = IForm()
+Application.Run(buttonForm)
+
+# Check to move files or delete them
+delMov = buttonForm.value
+
+# Store user specified path for backup directory
 directory = ""
+# Check if user selected backup
+if delMov == "Move to Folder":
+	# Create folder browser window 
+	dialog = FolderBrowserDialog()
+
+	# Record for user action and store selected path
+	if (dialog.ShowDialog() == DialogResult.OK):
+		directory = dialog.SelectedPath
 
 # Create folder browser window 
 dialog = FolderBrowserDialog()
@@ -34,63 +92,46 @@ dialog = FolderBrowserDialog()
 if (dialog.ShowDialog() == DialogResult.OK):
 	directory = dialog.SelectedPath
 
-# Retrieve all pathfiles and names from directory and subdirectories
-def retrieveFamilies(directory):
-	familiesNames = list()
+# Function to clean the backup files
+def directoryClean(directory):
+	# Empty list to store the filenames that have been removed or moved
+	modFiles = list()
+	# Collect all files in directory and subdirectories
 	for folderName, subFolders, files in os.walk(directory):
-		# Check if there are Revit families
-		families = re.compile(r"[^{ddd+}]\.rfa$")
+		# Pattern to check against if file is a revit family or project backup
+		matchBackups = re.compile(r'.*\.\d+.r(vt|fa)$')
+		# Loop over the files
 		for file in files:
-			# Assign matched files to a variable
-			matched = families.search(file)
+			# Assign the matched files to a variable
+			matched = matchBackups.search(file)
+			# If the file is a match
 			if matched:
-				# Get path of families
+				# Get the original filepath for the file
 				filePath = os.path.join(folderName, file)
-				familiesNames.append(filePath)
-	return familiesNames
+				# Change matched files permission to writable
+				os.chmod(filePath, 436)
+				# Append modified elements to modFiles list
+				modFiles.append(file)
+				# Case False: Move backup files
+				if delMov != "Remove":
+					# Get the destination filepath for the file (move will overwrite previous versions if full path is provided)
+					backupPath = os.path.join(backupDir, file)
+					# Move matched files
+					shutil.move(filePath, backupPath)
+				# Case True: Delete backup files
+				else:
+					# Delete matched files
+					os.unlink(filePath)
+	return modFiles
 
-# Call function to search for families in path and subfolders
-familiesList = retrieveFamilies(directory)
+# Call the function to execute
+cleanBackup = directoryClean(directory)
 
-# Store current document into variable
-app = __revit__.Application
-doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
+# Function to display result to user
+def printMessage(resultList, message):
+	if len(resultList) != 0:
+		print(message)
+		print("\n".join(resultList))
 
-# Class to define loading options, in 
-class familyLoadOptions(IFamilyLoadOptions):
-	def __init__(self, overwrite):
-		self.bool = overwrite
-	def OnFamilyFound(self, bool1, bool2):
-		return self.bool
-	def OnSharedFamilyFound(self, sfamily, bool1, familySource, bool2):
-		return self.bool
-
-# Create a individual transaction to change the parameters on sheet
-t = Transaction(doc, "Batch Load Families")
-# Start individual transaction
-t.Start()
-
-# Loop through families and load them into the project
-loadedFam = list()
-notLoadedFam = list()
-
-for family in familiesList:
-	try:
-		doc.LoadFamily(family, familyLoadOptions(True))
-		loadedFam.append(family)
-	except:
-		notLoadedFam.append(family)
-
-# Commit individual transaction
-t.Commit()
-
-# Print message to user about results
-if len(loadedFam) == 0:
-	print("No family has been loaded")
-else:
-	print("The following families have been loaded: \n")
-	print("\n".join(loadedFam))
-if len(notLoadedFam) != 0:
-	print("The following families load have failed: \n")
-	print("\n".join(notLoadedFam))
+# Print message
+printMessage(cleanBackup, "The following files has been changed:")
