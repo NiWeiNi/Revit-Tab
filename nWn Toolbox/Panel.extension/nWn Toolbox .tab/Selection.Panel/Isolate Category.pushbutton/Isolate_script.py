67e17f40cbc3fc387f37842bef22d9f9186094ad
Wei <weinini007@gmail.com>
2019-06-29 16:55:03 +1000

    feat: retrieve all categories in doc

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Selection.Panel/Isolate Category.pushbutton/Isolate_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Selection.Panel/Isolate Category.pushbutton/Isolate_script.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Isolates a category from selected and enters in isolated view mode.
+"""Isolates selected categories and overriden the rest of them with 90% transparency.
 """
 __title__ = 'Isolate\nCategory'
 __author__ = "nWn"
@@ -78,51 +78,9 @@ app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Collects all sheets in current document
-sheetsCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets) \
-                                                .ToElements()
+# Retrieve current view
+activeView = doc.ActiveView
 
+# Retrieve all categories in the document
+docCat = doc.Settings.Categories
 
-# Create a Transaction group to group all subsequent transactions
-tg = TransactionGroup(doc, "Update Drawn By and Checked By")
-
-# Start the group transaction
-tg.Start()
-
-# Create a individual transaction to change the parameters on sheet
-t = Transaction(doc, "Change Sheets Name")
-
-# Start individual transaction
-t.Start()
-
-# Variable to store modified sheets
-modSheets = list()
-
-# Define function to modify parameter
-def modParameter(param, checkEmpty, inputParam):
-    # Store the sheet number parameter in variable
-    sheetNumber = sheet.LookupParameter("Sheet Number").AsString()
-    # Retrieve sheet parameter
-    sheetDrawn = sheet.LookupParameter(param)
-    # Check if it is by default and input is not empty, set it with user input
-    if sheetDrawn.AsString() == checkEmpty and inputParam != "":
-        sheetDrawn.Set(inputParam)
-        # Check if the sheet has been previously modified and append to list
-        if sheetNumber not in modSheets:
-            modSheets.append(sheetNumber)
-
-# Loop through all sheets
-for sheet in sheetsCollector:
-    # Run function to modify Drawer
-    modParameter("Drawn By", "Author", nameDrawer)
-    # Call function to modify Checker
-    modParameter("Checked By", "Checker", nameChecker)
-
-# Commit individual transaction
-t.Commit()
-
-# Combine all individual transaction in the group transaction
-tg.Assimilate()
-
-# Print all changed sheets
-print("The following sheets have been modified: \n\n" + "\n".join(modSheets))
