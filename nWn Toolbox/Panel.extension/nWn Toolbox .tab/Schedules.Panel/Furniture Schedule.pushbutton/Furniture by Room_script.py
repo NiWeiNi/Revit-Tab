5540ce3c672360c24c1b0930b499acc22873e2d5
Wei <weinini007@gmail.com>
2019-07-30 16:19:06 +1000

    feat: fill in parameter in furnitures

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Schedules.Panel/Furniture Schedule.pushbutton/Furniture by Room_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Schedules.Panel/Furniture Schedule.pushbutton/Furniture by Room_script.py
@@ -40,53 +40,27 @@ for phase in phases:
 	if phase.Name == phasesForm:
 		phaseObject = phase
 
-# Retrieve room in where the furniture is located
-for f in furnitureCollector:
-	f.Room[phaseObject]
-
-"""
-# Create a Transaction group to group all subsequent transactions
-tg = TransactionGroup(doc, "Update Drawn By and Checked By")
-
-# Start the group transaction
-tg.Start()
-
-# Create a individual transaction to change the parameters on sheet
-t = Transaction(doc, "Change Sheets Name")
-
+# Create a individual transaction
+t = Transaction(doc, "Fill Furniture Room Parameter")
 # Start individual transaction
 t.Start()
 
-# Variable to store modified sheets
-modSheets = list()
-
-# Define function to modify parameter
-def modParameter(param, checkEmpty, inputParam):
-    # Store the sheet number parameter in variable
-    sheetNumber = sheet.LookupParameter("Sheet Number").AsString()
-    # Retrieve sheet parameter
-    sheetDrawn = sheet.LookupParameter(param)
-    # Check if it is by default and input is not empty, set it with user input
-    if sheetDrawn.AsString() == checkEmpty and inputParam != "":
-        sheetDrawn.Set(inputParam)
-        # Check if the sheet has been previously modified and append to list
-        if sheetNumber not in modSheets:
-            modSheets.append(sheetNumber)
-
-# Loop through all sheets
-for sheet in sheetsCollector:
-    # Run function to modify Drawer
-    modParameter("Drawn By", "Author", nameDrawer)
-    # Call function to modify Checker
-    modParameter("Checked By", "Checker", nameChecker)
+# Retrieve room in where the furniture is located and set in furniture
+param = "In Room"
+message = ""
+for f in furnitureCollector:
+	try:
+		roomNumber = f.Room[phaseObject].LookupParameter("Number").AsString()
+	except:
+		roomNumber = ""
+	try:
+		f.LookupParameter(param).Set(roomNumber)
+	except:
+		message = "Please add " + param + " parameter to project."
 
 # Commit individual transaction
 t.Commit()
 
-# Combine all individual transaction in the group transaction
-tg.Assimilate()
-
-# Print all changed sheets
-print("The following sheets have been modified: \n\n" + "\n".join(modSheets))
-
-"""
+# Print result
+if len(message) > 0:
+	print(message)
