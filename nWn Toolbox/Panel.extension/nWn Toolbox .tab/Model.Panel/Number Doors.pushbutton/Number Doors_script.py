d9e659314c2d69fa0bc1a2e8e4322e91476bf824
NiweiNi <wnini@hotmail.es>
2019-10-15 14:47:43 +1100

    feat: Retrieve all doors and phases

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Doors.pushbutton/Number Doors_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Doors.pushbutton/Number Doors_script.py
@@ -1,64 +1,29 @@
 # -*- coding: utf-8 -*-
-"""Dimension grids.
+"""Number doors according to room number.
 """
-__title__ = 'Dimension\nGrids'
 __author__ = "nWn"
+__title__ = "Number\n Doors"
 
-# Import commom language runtime
-import clr
-# Import Revit API
-from Autodesk.Revit.DB import FilteredElementCollector, ElementCategoryFilter, \
-							BuiltInCategory, ReferenceArray, Reference, Transaction, \
-							TransactionGroup, Line
-# Import Revit API
-from Autodesk.Revit.Creation.ItemFactoryBase import NewDimension
+# Import 
+from pyrevit import revit, DB
+from pyrevit import script
+from pyrevit import forms
 
 # Store current document into variable
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Get current view
-view = doc.ActiveView
+# Select all doors
+doorsFilter = DB.ElementCategoryFilter(DB.BuiltInCategory.OST_Doors)
+doorsCollector = DB.FilteredElementCollector(doc).WherePasses(doorsFilter).WhereElementIsNotElementType()
 
-# Select all grids by filter
-gridsFilter = ElementCategoryFilter(BuiltInCategory.OST_Grids)
-gridsCollector = FilteredElementCollector(doc).WherePasses(gridsFilter) \
-				.WhereElementIsNotElementType()
+# Select all phases
+phases = doc.Phases
+phasesName = [ph.Name for ph in phases]
 
-# Variables to split grids into columns and rows
-gridsColumn = ReferenceArray()
-gridsRow = ReferenceArray()
-gridsC = []
-gridsR = []
+# Class 
 
-# Check grids name and split in groups
-for grid in gridsCollector:
-	gridName = grid.LookupParameter("Name").AsString()
-	if any(char.isdigit() for char in gridName):
-		gridsColumn.Append(Reference(grid))
-		gridsC.append(grid)
-	else:
-		gridsRow.Append(Reference(grid))
-		gridsR.append(grid)
+# Form to select phase
+phaseForm = forms.SelectFromList.show(phasesName, title = "Select Phase")
 
-# Retrieve endpoints
-endPointC0 = gridsC[0].Curve.GetEndPoint(0)
-endPointC1 = gridsC[-1].Curve.GetEndPoint(0)
-
-endPointR0 = gridsR[0].Curve.GetEndPoint(0)
-endPointR1 = gridsR[-1].Curve.GetEndPoint(0)
-
-# Create line to place dimension
-lineC = Line.CreateBound(endPointC0, endPointC1)
-lineR = Line.CreateBound(endPointR0, endPointR1)
-
-# Create transaction to create dimensions
-t = Transaction(doc, "Dimension grids")
-t.Start()
-
-# Create dimensions
-doc.Create.NewDimension(view, lineC, gridsColumn)
-doc.Create.NewDimension(view, lineR, gridsRow)
-
-# Commit transaction
-t.Commit()
+print phaseForm
