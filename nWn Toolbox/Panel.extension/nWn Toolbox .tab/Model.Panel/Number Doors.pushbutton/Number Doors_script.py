316b42ade3925d8edb26a9c800f75953ea65e85f
NiweiNi <wnini@hotmail.es>
2019-10-16 14:50:07 +1100

    refactor: define as function

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Doors.pushbutton/Number Doors_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Doors.pushbutton/Number Doors_script.py
@@ -5,7 +5,7 @@ __author__ = "nWn"
 __title__ = "Number\n Doors"
 
 # Import 
-from pyrevit import revit, DB, UI
+from pyrevit import revit, DB
 from pyrevit import script
 from pyrevit import forms
 
@@ -13,76 +13,78 @@ from pyrevit import forms
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Set the condition to run the script: Doors must have Department parameter
-params = doc.ParameterBindings.ForwardIterator()
-while params.MoveNext():
-	for cat in params.Current.Categories:
-		if cat.Name == "Doors" and params.Key.Name == "Department":
-			#TODO
-			continue
-		else:
-			forms.alert("Please create a Project Parameter for Doors called Department", ok = True, exitscript= True)
+def numberDoors():
+	# Set the condition to run the script: Doors must have Department parameter
+	params = doc.ParameterBindings.ForwardIterator()
+	while params.MoveNext():
+		for cat in params.Current.Categories:
+			if cat.Name == "Doors" and params.Key.Name == "Department":
+				# Select all doors
+				doorsFilter = DB.ElementCategoryFilter(DB.BuiltInCategory.OST_Doors)
+				doorsCollector = DB.FilteredElementCollector(doc).WherePasses(doorsFilter).WhereElementIsNotElementType()
 
-"""
-# Select all doors
-doorsFilter = DB.ElementCategoryFilter(DB.BuiltInCategory.OST_Doors)
-doorsCollector = DB.FilteredElementCollector(doc).WherePasses(doorsFilter).WhereElementIsNotElementType()
+				# Select all phases
+				phases = doc.Phases
+				phasesName = [ph.Name for ph in phases]
 
-# Select all phases
-phases = doc.Phases
-phasesName = [ph.Name for ph in phases]
+				# Form to select phase
+				phaseForm = forms.SelectFromList.show(phasesName, title = "Select Phase")
 
-# Form to select phase
-phaseForm = forms.SelectFromList.show(phasesName, title = "Select Phase")
+				# Retrieve selected phase
+				for ph in phases:
+					if ph.Name == phaseForm:
+						selectedPhase = ph
 
-# Retrieve selected phase
-for ph in phases:
-	if ph.Name == phaseForm:
-		selectedPhase = ph
+				# Check doors ToRoom
+				toRooms = []
+				for d in doorsCollector:
+					room = d.ToRoom[selectedPhase]
+					toRooms.append(room)
 
-# Check doors ToRoom
-toRooms = []
-for d in doorsCollector:
-	room = d.ToRoom[selectedPhase]
-	toRooms.append(room)
+				# Set auxiliar variables
+				roomNumbers = [x.Number if x != None else "" for x in toRooms]
+				countNumbers = {}
+				finalList = []
+				department = []
 
-# Set auxiliar variables
-roomNumbers = [x.Number if x != None else "" for x in toRooms]
-countNumbers = {}
-finalList = []
-department = []
+				# Loop through all  rooms
+				for r, d in zip(toRooms, doorsCollector):
+					# Check room is not null
+					if r != None:
+						department.append(r.LookupParameter("Department").AsString())
+						# Check room is not duplicated
+						if r.Number not in countNumbers.keys():
+							finalList.append(r.Number)
+							countNumbers[r.Number] = 1
+						# If room is duplicated, count the number
+						else:
+							countNumbers[r.Number] = countNumbers[r.Number] + 1
+							if countNumbers[r.Number] == 2:
+								finalList[finalList.index(r.Number)] = r.Number + "A"
+								finalList.append(r.Number + "B")
+							else:
+								finalList.append(r.Number + chr(ord('@') + countNumbers[r.Number]))
+					else:
+						finalList.append(d.LookupParameter("Mark").AsString())
+						department.append("")
 
-# Loop through all  rooms
-for r, d in zip(toRooms, doorsCollector):
-	# Check room is not null
-	if r != None:
-		department.append(r.LookupParameter("Department").AsString())
-		# Check room is not duplicated
-		if r.Number not in countNumbers.keys():
-			finalList.append(r.Number)
-			countNumbers[r.Number] = 1
-		# If room is duplicated, count the number
-		else:
-			countNumbers[r.Number] = countNumbers[r.Number] + 1
-			if countNumbers[r.Number] == 2:
-				finalList[finalList.index(r.Number)] = r.Number + "A"
-				finalList.append(r.Number + "B")
-			else:
-				finalList.append(r.Number + chr(ord('@') + countNumbers[r.Number]))
-	else:
-		finalList.append(d.LookupParameter("Mark").AsString())
-		department.append("")
+				# Create a individual transaction to change the parameters
+				t = DB.Transaction(doc, "Set Mark on Doors")
+				# Start individual transaction
+				t.Start()
 
-# Create a individual transaction to change the parameters
-t = DB.Transaction(doc, "Set Mark on Doors")
-# Start individual transaction
-t.Start()
+				# Set numbers to doors
+				for d, n, dep in zip(doorsCollector, finalList, department):
+					d.LookupParameter("Mark").Set(n)
+					# d.LookupParameter("Department").Set(dep)
 
-# Set numbers to doors
-for d, n, dep in zip(doorsCollector, finalList, department):
-	d.LookupParameter("Mark").Set(n)
-	d.LookupParameter("Department").Set(dep)
+				# Commit transaction
+				t.Commit()
+				
+				return True
 
-# Commit transaction
-t.Commit()
-"""
+	# Finish script if there is no required project parameter 
+	forms.alert("Please create a Project Parameter for Doors called Department", ok = True, exitscript= True)
+
+# Call function to number doors
+numberDoors()
