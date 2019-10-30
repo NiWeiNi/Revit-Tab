d8fa62c1ad4197436592a970602ad70debfd0000
NiweiNi <wnini@hotmail.es>
2019-10-31 10:58:53 +1100

    feat: list all selected elements name

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Selection.Panel/List Selection.pushbutton/List Selection_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Selection.Panel/List Selection.pushbutton/List Selection_script.py
@@ -12,3 +12,20 @@ from pyrevit import forms
 # Store current document into variable
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+# Retrieve selected elements Ids
+selection = uidoc.Selection
+selectionIds = selection.GetElementIds()
+
+# Check if Selection is empty
+count = selectionIds.Count 
+if count == 0:
+	print "No element selected"
+# Print selected elements
+else:
+	for id in selectionIds:
+		try:
+			e = doc.GetElement(id)
+			print e.Name
+		except:
+			print ""
