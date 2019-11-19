d1771977531a75e45d9fb676856d9b30d4ddfe33
NiweiNi <wnini@hotmail.es>
2019-11-19 14:52:26 +1100

    feat: retrieve param to filter from view

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Strip Model.pushbutton/Strip Model_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Strip Model.pushbutton/Strip Model_script.py
@@ -14,3 +14,11 @@ from pyrevit import forms
 # Store current document into variable
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+# Collect all views and sheets
+viewsCollector = DB.FilteredElementCollector(doc).OfClass(DB.View)
+sheetsCollector = DB.FilteredElementCollector(doc).OfClass(DB.ViewSheet)
+
+# Select parameter in view
+view = [x for x in viewsCollector if x.ViewType == DB.ViewType.FloorPlan][0]
+param = [p for p in view.Parameters if "View Set" in p.Definition.Name][0]
