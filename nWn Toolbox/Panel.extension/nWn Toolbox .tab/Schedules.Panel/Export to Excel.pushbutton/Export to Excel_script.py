98c5e2f762e2d48a9797909c8fc8aca28a5133dd
NiweiNi <wnini@hotmail.es>
2019-11-20 12:29:03 +1100

    feat: retrieve data from schedule

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Schedules.Panel/Export to Excel.pushbutton/Export to Excel_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Schedules.Panel/Export to Excel.pushbutton/Export to Excel_script.py
@@ -21,8 +21,21 @@ views = [v for v in viewsCollector if not v.IsTemplate]
 schedules = [x for x in views if "<Revision Schedule>".lower() not in x.Name.lower() and x.ViewType == DB.ViewType.Schedule]
 schedNames = [x.Name for x in schedules]
 
-# Create form to display schedule to export
+# Create form to display schedule to export and retieve schedule
 selectedSched = forms.SelectFromList.show(schedNames, button_name="Select Schedule to Export")
+sched = [x for x in schedules if x.Name == selectedSched][0]
 
+# Retrieve data from the schedule
+tableData = sched.GetTableData()
+sectionData = tableData.GetSectionData(DB.SectionType.Body)
+numbRows = sectionData.NumberOfRows
+numbCols = sectionData.NumberOfColumns
 
-
+data = []
+# Create list with schedule data with rows as second level lists
+for i in range(numbRows):
+    rows = []
+    for j in range(numbCols):
+        content = sched.GetCellText(DB.SectionType.Body, i, j)
+        rows.append(content)
+    data.append(rows)
