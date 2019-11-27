c806fb86153ccda0cd6199f82a0c02b48ff6ac13
NiweiNi <wnini@hotmail.es>
2019-11-27 12:01:31 +1100

    feat: delete unselected views from the  model

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Strip Model.pushbutton/Strip Model_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Strip Model.pushbutton/Strip Model_script.py
@@ -30,4 +30,30 @@ sheetsCollector = DB.FilteredElementCollector(doc).OfClass(DB.ViewSheet)
 
 # Select parameter in view
 view = [x for x in viewsCollector if x.ViewType == DB.ViewType.FloorPlan][0]
-param = [p for p in view.Parameters if "View Set" in p.Definition.Name][0]
+param = [p.Definition.Name for p in view.Parameters if "View Set" in p.Definition.Name][0]
+
+# Set view set to select
+viewSet = [x.LookupParameter(param).AsString() for x in viewsCollector if x.LookupParameter(param) != None]
+selectSet = sorted(set(viewSet))
+
+# Display select view sets form
+vSet = forms.SelectFromList.show(selectSet, "View Set to Keep", 600, 600, multiselect=True)
+
+# Classify views
+viewsIdDelete = [x.Id for x in viewsCollector if x.LookupParameter(param) == None or \
+                x.LookupParameter(param).AsString() not in vSet]
+viewsIdKeep = [x.Id for x in viewsCollector if x.LookupParameter(param) != None and \
+                x.LookupParameter(param).AsString() in vSet]
+
+# Create single transaction and start it
+t = DB.Transaction(doc, "Delete elements")
+t.Start()
+
+for v in viewsIdDelete:
+    try:
+        doc.Delete(v)
+    except:
+        pass
+
+# Commit transaction
+t.Commit()
