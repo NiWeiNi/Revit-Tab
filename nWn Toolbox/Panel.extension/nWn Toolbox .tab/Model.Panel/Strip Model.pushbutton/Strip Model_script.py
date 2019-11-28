eebbe591cb62011932d4af480ac9eacb0feac97a
NiweiNi <wnini@hotmail.es>
2019-11-28 14:14:59 +1100

    feat: add progress bar

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Strip Model.pushbutton/Strip Model_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Strip Model.pushbutton/Strip Model_script.py
@@ -30,76 +30,94 @@ def checkCentral():
     else:
         return False
 
-# Check if model is linked to central
-if checkCentral():
-    # Finish execution script if model is not detached
-    forms.alert("Please detach model from central and save it to your local drive.", ok = True, exitscript= True)
+# Create progress bar
+count = 1
+finalCount = 0
+with forms.ProgressBar(step=10) as pb:
 
-# Collect all views and sheets
-viewsCollector = DB.FilteredElementCollector(doc).OfClass(DB.View)
-sheetsCollector = DB.FilteredElementCollector(doc).OfClass(DB.ViewSheet)
+    # Check if model is linked to central
+    if checkCentral():
+        # Finish execution script if model is not detached
+        forms.alert("Please detach model from central and save it to your local drive.", ok = True, exitscript= True)
 
-# Select parameter in view
-view = [x for x in viewsCollector if x.ViewType == DB.ViewType.FloorPlan][0]
-param = [p.Definition.Name for p in view.Parameters if "View Set" in p.Definition.Name][0]
+    # Collect all views and sheets
+    viewsCollector = DB.FilteredElementCollector(doc).OfClass(DB.View)
+    sheetsCollector = DB.FilteredElementCollector(doc).OfClass(DB.ViewSheet)
 
-# Set view set to select
-viewSet = [x.LookupParameter(param).AsString() for x in viewsCollector if x.LookupParameter(param) != None]
-selectSet = sorted(set(viewSet))
+    # Select parameter in view
+    view = [x for x in viewsCollector if x.ViewType == DB.ViewType.FloorPlan][0]
+    param = [p.Definition.Name for p in view.Parameters if "View Set" in p.Definition.Name][0]
 
-# Display select view sets form
-vSet = forms.SelectFromList.show(selectSet, "View Set to Keep", 600, 600, multiselect=True)
+    # Set view set to select
+    viewSet = [x.LookupParameter(param).AsString() for x in viewsCollector if x.LookupParameter(param) != None]
+    selectSet = sorted(set(viewSet))
 
-# Classify views
-viewsIdDelete = [x.Id for x in viewsCollector if x.LookupParameter(param) == None or \
-                x.LookupParameter(param).AsString() not in vSet]
-viewsIdKeep = [x.Id for x in viewsCollector if x.LookupParameter(param) != None and \
-                x.LookupParameter(param).AsString() in vSet]
+    # Display select view sets form
+    vSet = forms.SelectFromList.show(selectSet, "View Set to Keep", 600, 600, multiselect=True)
 
-# Prompt form to check if user wants to keep annotations
-delAnnotations = forms.alert("Confirm to delete annotation elements", title="Delete annotations?", yes=True, no=True)
+    # Classify views
+    viewsIdDelete = [x.Id for x in viewsCollector if x.LookupParameter(param) == None or \
+                    x.LookupParameter(param).AsString() not in vSet]
+    viewsIdKeep = [x.Id for x in viewsCollector if x.LookupParameter(param) != None and \
+                    x.LookupParameter(param).AsString() in vSet]
 
-# Create group transaction
-tg = DB.TransactionGroup(doc, "Delete elements in document")
-# Start group transaction
-tg.Start()
-# Create single transaction and start it
-t = DB.Transaction(doc, "Delete views")
-t.Start()
+    # Prompt form to check if user wants to keep annotations
+    delAnnotations = forms.alert("Confirm to delete annotation elements", title="Delete annotations?", yes=True, no=True)
 
-for v in viewsIdDelete:
-    try:
-        doc.Delete(v)
-    except:
-        pass
+    # Update progress bar
+    pb.update_progress(count, finalCount)
+    count += 1
 
-# Commit transaction
-t.Commit()
+    # Create group transaction
+    tg = DB.TransactionGroup(doc, "Delete elements in document")
+    # Start group transaction
+    tg.Start()
 
-# Check if annotation elements must be deleted
-if delAnnotations:
-    # Collect annotation categories to be deleted
-    annoElements = []
-    categories = doc.Settings.Categories
-    for cat in categories:
-        if cat.CategoryType == DB.CategoryType.Annotation:
-            collector = DB.FilteredElementCollector(doc).OfCategoryId(cat.Id)
-            elemIds = [x.Id for x in collector]
-            annoElements = annoElements + elemIds
-    annoIds = List[DB.ElementId](annoElements)
+    # Check if annotation elements must be deleted
+    if delAnnotations:
+        # Collect annotation categories to be deleted
+        annoElements = []
+        categories = doc.Settings.Categories
+        for cat in categories:
+            if cat.CategoryType == DB.CategoryType.Annotation:
+                collector = DB.FilteredElementCollector(doc).OfCategoryId(cat.Id)
+                elemIds = [x.Id for x in collector]
+                annoElements = annoElements + elemIds
+        annoIds = List[DB.ElementId](annoElements)
 
-# Create single transaction and start it
-t1 = DB.Transaction(doc, "Delete annotation elements")
-t1.Start()
+    finalCount = len(annoElements + viewsIdDelete)
 
-# Delete all annotation elements
-for i in annoIds:
-    try:
-        doc.Delete(i)
-    except:
-        pass
+    # Create single transaction and start it
+    t = DB.Transaction(doc, "Delete views")
+    t.Start()
 
-# Commit transaction
-t1.Commit()
-# Commit group transaction
-tg.Commit()
+    for v in viewsIdDelete:
+        try:
+            doc.Delete(v)
+        except:
+            pass
+        # Update progress bar
+        pb.update_progress(count, finalCount)
+        count += 1
+
+    # Commit transaction
+    t.Commit()
+
+    # Create single transaction and start it
+    t1 = DB.Transaction(doc, "Delete annotation elements")
+    t1.Start()
+
+    # Delete all annotation elements
+    for i in annoIds:
+        try:
+            doc.Delete(i)
+        except:
+            pass
+        # Update progress bar
+        pb.update_progress(count, finalCount)
+        count += 1
+
+    # Commit transaction
+    t1.Commit()
+    # Commit group transaction
+    tg.Commit()
