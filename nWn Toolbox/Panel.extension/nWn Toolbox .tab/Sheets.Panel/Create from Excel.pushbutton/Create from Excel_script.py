4026c5717d07bc86b6c1c0a3b71de7ec46b8ce3b
NiweiNi <wnini@hotmail.es>
2019-11-29 15:02:47 +1100

    feat: read excel

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Create from Excel.pushbutton/Create from Excel_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Create from Excel.pushbutton/Create from Excel_script.py
@@ -12,8 +12,35 @@ from pyrevit import script
 from pyrevit import forms
 
 # Import libraries
-import xlsxwriter
+import clr
+clr.AddReference("Microsoft.Office.Interop.Excel")
+from Microsoft.Office.Interop import Excel
+from System.Runtime.InteropServices import Marshal
 
 # Store current document into variable
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+# Prompt user to specify file path
+pathFile = forms.pick_file(files_filter='Excel Workbook (*.xlsx)|*.xlsx|''Excel 97-2003 Workbook|*.xls')
+
+# Create Excel object
+excel = Excel.ApplicationClass()
+excel = Marshal.GetActiveObject("Excel.Application")
+workbook = excel.Workbooks.Open(pathFile)
+ws = workbook.Worksheets[1]
+
+# Read data
+sNumber = ws.Range["A1", "A1000"]
+sName = ws.Range["B1", "B1000"]
+
+print sNumber.Value2
+print sName.Value2
+
+excel.ActiveWorkbook.Close(False)
+Marshal.ReleaseComObject(ws)
+Marshal.ReleaseComObject(workbook)
+Marshal.ReleaseComObject(excel)
+
+for n in sNumber.Value2:
+    print n
