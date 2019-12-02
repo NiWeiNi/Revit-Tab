898fbdc8edcf9ef2a88c06e993ea7870a584decd
Wei <weinini007@gmail.com>
2019-12-02 13:16:37 +1100

    refactor: form and pyrevit imports

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Place View.pushbutton/Place View_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Place View.pushbutton/Place View_script.py
@@ -2,75 +2,21 @@
 """Place current active view in selected sheet.
 
 NOTE: 
-Position will be center."""
+Position will be at center of sheet."""
 __title__ = 'Current View\non Sheet'
 __author__ = "nWn"
 
-# Import commom language runtime
-import clr
-
-# Import forms from pyrevit
+# Import 
+from pyrevit import revit, DB
+from pyrevit import script
 from pyrevit import forms
 
-# Import Revit DB
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, \
-                            Transaction, TransactionGroup
-
 # Store current document to variable
-app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Collects all sheets in current document
-sheetsCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets) \
-                                                .ToElements()
+# Form to select sheet
+sheets = forms.select_sheets()
 
 # Retrieve current view
 curView = doc.ActiveView
-
-
-"""
-# Create a Transaction group to group all subsequent transactions
-tg = TransactionGroup(doc, "Update Drawn By and Checked By")
-
-# Start the group transaction
-tg.Start()
-
-# Create a individual transaction to change the parameters on sheet
-t = Transaction(doc, "Change Sheets Name")
-
-# Start individual transaction
-t.Start()
-
-# Variable to store modified sheets
-modSheets = list()
-
-# Define function to modify parameter
-def modParameter(param, checkEmpty, inputParam):
-    # Store the sheet number parameter in variable
-    sheetNumber = sheet.LookupParameter("Sheet Number").AsString()
-    # Retrieve sheet parameter
-    sheetDrawn = sheet.LookupParameter(param)
-    # Check if it is by default and input is not empty, set it with user input
-    if sheetDrawn.AsString() == checkEmpty and inputParam != "":
-        sheetDrawn.Set(inputParam)
-        # Check if the sheet has been previously modified and append to list
-        if sheetNumber not in modSheets:
-            modSheets.append(sheetNumber)
-
-# Loop through all sheets
-for sheet in sheetsCollector:
-    # Run function to modify Drawer
-    modParameter("Drawn By", "Author", nameDrawer)
-    # Call function to modify Checker
-    modParameter("Checked By", "Checker", nameChecker)
-
-# Commit individual transaction
-t.Commit()
-
-# Combine all individual transaction in the group transaction
-tg.Assimilate()
-
-# Print all changed sheets
-print("The following sheets have been modified: \n\n" + "\n".join(modSheets))
-"""
