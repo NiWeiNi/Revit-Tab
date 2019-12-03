65ce4ee57a70239bb120907cb53ffc628da46e65
NiweiNi <wnini@hotmail.es>
2019-12-03 12:19:08 +1100

    feat: retrieve data from excel

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Create from Excel.pushbutton/Create from Excel_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Create from Excel.pushbutton/Create from Excel_script.py
@@ -21,6 +21,20 @@ from System.Runtime.InteropServices import Marshal
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
+# Function to retrieve data
+def retData(array1, array2):
+    data1 = []
+    data2 = []
+    for a1, a2 in zip(array1, array2):
+        # Check that data is not empty
+        if a1 == None and a2 ==None:
+            break
+        else:
+            data1.append(a1)
+            data2.append(a2)
+    sheets = {"sheetNumbers" : data1, "sheetNames" : data2}
+    return sheets
+
 # Prompt user to specify file path
 pathFile = forms.pick_file(files_filter='Excel Workbook (*.xlsx)|*.xlsx|''Excel 97-2003 Workbook|*.xls')
 
@@ -34,13 +48,14 @@ ws = workbook.Worksheets[1]
 sNumber = ws.Range["A1", "A1000"]
 sName = ws.Range["B1", "B1000"]
 
-print sNumber.Value2
-print sName.Value2
+sheetNumbers = retData(sNumber.Value2, sName.Value2)["sheetNumbers"][1:]
+sheetNames = retData(sNumber.Value2, sName.Value2)["sheetNames"][1:]
 
+"""
 excel.ActiveWorkbook.Close(False)
 Marshal.ReleaseComObject(ws)
 Marshal.ReleaseComObject(workbook)
 Marshal.ReleaseComObject(excel)
-
-for n in sNumber.Value2:
-    print n
+"""
+excel.Workbooks.close()
+excel.quit()
