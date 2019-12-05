909035147420b203463327db275505eb78195559
NiweiNi <wnini@hotmail.es>
2019-12-05 11:19:02 +1100

    refactor: filter rooms inside functions

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Rooms by Spline.pushbutton/Number Rooms_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Rooms by Spline.pushbutton/Number Rooms_script.py
@@ -17,26 +17,33 @@ uidoc = __revit__.ActiveUIDocument
 
 # Retrieve all rooms
 roomsCollector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Rooms)
+# Dispose not placed and redundant rooms
+roomsF = [x for x in roomsCollector if x.Area != 0.0]
 # Retrieve levels
 levels = DB.FilteredElementCollector(doc).OfClass(DB.Level)
 levelNames = [x.Name for x in levels]
 # Retrieve lines
 lines = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Lines)
 
-# Clasify rooms by level name
-roomsDict = {}
-for r in roomsCollector:
-    lvName = r.Level.Name
-    if lvName not in roomsDict.keys():
-        roomsDict[lvName] = [r]
-    else:
-        roomsDict[lvName].append(r)
+# Function to clasify rooms by level name
+def roomSorted(roomsCollector):
+    roomsDict = {}
+    for r in roomsCollector:
+        lvName = r.Level.Name
+        if lvName not in roomsDict.keys():
+            roomsDict[lvName] = [r]
+        else:
+            roomsDict[lvName].append(r)
+    return roomsDict
 
 # Function to retrieve rooms by level name
-def roomsBylevel(levelName):
+def roomsBylevel(levelName, rooms):
     if levelName in roomsDict.keys():
-        rooms = roomsDict[lvN]
-        return rooms
+        roomsL = rooms[lvN]
+        return roomsL
+
+# Rooms by level name
+roomsDict = roomSorted(roomsF)
 
 # Options for solid intersection
 optSol = DB.SolidCurveIntersectionOptions()
@@ -47,19 +54,22 @@ spOpt = DB.SpatialElementBoundaryOptions()
 # Retrieve line to renumber elements
 interSeg = []
 for lvN in levelNames:
-    rooms = roomsBylevel(lvN)
-    for l in lines:
-        if l.LookupParameter("Work Plane") != None:
-            wP = l.LookupParameter("Work Plane").AsString()
-            lineName = l.LineStyle.Name
-            if wP == "Level : " + lvN and lineName == "Room Number Line":
-                line = l.GeometryCurve
-    # Obtain intersection between room and curve
-    for r in rooms:
-        # Spatial element to solid
-        calc = DB.SpatialElementGeometryCalculator(doc, spOpt)
-        rCalc = calc.CalculateSpatialElementGeometry(r)
-        rGeo = rCalc.GetGeometry()
-        # Get the intersection of curve and solid
-        inter = rGeo.IntersectWithCurve(line, optSol)
-        interSeg.append(inter)
+    rooms = roomsBylevel(lvN, roomsDict)
+    # Check there are rooms in the level
+    if rooms != None:
+        for l in lines:
+            if l.LookupParameter("Work Plane") != None:
+                wP = l.LookupParameter("Work Plane").AsString()
+                lineName = l.LineStyle.Name
+                if wP == "Level : " + lvN and lineName == "Room Number Line":
+                    line = l.GeometryCurve
+        # Obtain intersection between room and curve
+        for r in rooms:
+            # Spatial element to solid
+            calc = DB.SpatialElementGeometryCalculator(doc, spOpt)
+            rCalc = calc.CalculateSpatialElementGeometry(r)
+            rGeo = rCalc.GetGeometry()
+            # Get the intersection of curve and solid
+            inter = rGeo.IntersectWithCurve(line, optSol)
+            interSeg.append(inter)
+            
