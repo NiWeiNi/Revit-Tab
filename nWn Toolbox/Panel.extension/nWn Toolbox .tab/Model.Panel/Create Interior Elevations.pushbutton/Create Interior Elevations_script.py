cd0171feed000ae93996b8d7d1a0483014e5a7c9
NiweiNi <wnini@hotmail.es>
2019-12-06 11:41:38 +1100

    feat: prompt user form to select rooms

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Create Interior Elevations.pushbutton/Create Interior Elevations_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Create Interior Elevations.pushbutton/Create Interior Elevations_script.py
@@ -14,3 +14,15 @@ from pyrevit import forms
 # Store current document into variable
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+# Function to display select room number
+def roomNumber():
+	# Collect all rooms
+	rooms = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Rooms)
+	# Create set of rooms
+	roomNumber = [r.LookupParameter("Number").AsString() + "    ---    " + r.LookupParameter("Name").AsString() for r in rooms]
+	# Prompt selection list
+	return forms.SelectFromList.show(roomNumber, "Rooms to elevate", 600, 600, multiselect = True)
+
+# Prompt to select rooms
+roomNumber()
