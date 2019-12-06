b57243684e07e712ab9033fe6738adf974112aed
NiweiNi <wnini@hotmail.es>
2019-12-06 11:27:23 +1100

    feat: modify room numbers

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Rooms by Spline.pushbutton/Number Rooms_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Number Rooms by Spline.pushbutton/Number Rooms_script.py
@@ -47,6 +47,17 @@ def changeRoomNumber(room, number):
     number = str(number)
     room.LookupParameter("Number").Set(number)
 
+# Function to map number between 0 and 1
+def mapNumbers(listConvert):
+    listScaled = []
+    # Span of list to convert
+    convSpan = listConvert[-1] - listConvert[0] * 1.0
+    # Convert number into a 0-1 range and append to output list
+    for e in listConvert:
+        valueScaled = e / convSpan
+        listScaled.append(valueScaled)
+    return listScaled
+
 # Rooms by level name
 roomsDict = roomSorted(roomsF)
 
@@ -55,29 +66,6 @@ optSol = DB.SolidCurveIntersectionOptions()
 
 # Options for spatial elements
 spOpt = DB.SpatialElementBoundaryOptions()
-"""
-# Retrieve line to renumber elements
-interSeg = []
-for lvN in levelNames:
-    rooms = roomsBylevel(lvN, roomsDict)
-    # Check there are rooms in the level
-    if rooms != None:
-        for l in lines:
-            if l.LookupParameter("Work Plane") != None:
-                wP = l.LookupParameter("Work Plane").AsString()
-                lineName = l.LineStyle.Name
-                if wP == "Level : " + lvN and lineName == "Room Number Line":
-                    line = l.GeometryCurve
-        # Obtain intersection between room and curve
-        for r in rooms:
-            # Spatial element to solid
-            calc = DB.SpatialElementGeometryCalculator(doc, spOpt)
-            rCalc = calc.CalculateSpatialElementGeometry(r)
-            rGeo = rCalc.GetGeometry()
-            # Get the intersection of curve and solid
-            inter = rGeo.IntersectWithCurve(line, optSol)
-            interSeg.append(inter)
-"""         
 
 # Retrieve line to renumber elements
 interSeg = []
@@ -94,17 +82,24 @@ for lvN in levelNames:
                     print line.IsBound
         # Obtain intersection between room and curve
         numbRooms = len(rooms)
+        roomSort = {}
         for r in rooms:
-            for norParam, number in zip(range(0, numbRooms * 4), range(numbRooms)):
+            parMap = mapNumbers(range(0, numbRooms * 4))
+            for norParam in parMap:
                 # Check if point in curve is inside room
-                cPoint = line.Evaluate(norParam/1000.0, True)
+                cPoint = line.Evaluate(norParam, True)
                 if r.IsPointInRoom(cPoint):
-                    # Create single transaction and start it
-                    t = DB.Transaction(doc, "Renumber Door")
-                    t.Start()
-                    print "Inside!"
-                    r.LookupParameter("Room")
-                    changeRoomNumber(r, number)
-                     # Commit transaction
-                    t.Commit()
+                    roomSort[norParam] = r
                     break
+        # Create single transaction and start it
+        t = DB.Transaction(doc, "Renumber Door")
+        t.Start()
+        
+        # Keys sorted
+        keySorted = sorted(roomSort.keys())
+        # Chnage door parameters
+        for k, n in zip(keySorted, range(numbRooms)):
+            changeRoomNumber(roomSort[k], n)
+
+        # Commit transaction
+        t.Commit()
