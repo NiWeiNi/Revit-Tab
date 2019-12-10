29f7ecd162d67d0fca330a9008baf120bdd8fdaa
NiweiNi <wnini@hotmail.es>
2019-12-10 11:22:22 +1100

    refactor: wrapped rooms for selection

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Create Interior Elevations.pushbutton/Create Interior Elevations_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Model.Panel/Create Interior Elevations.pushbutton/Create Interior Elevations_script.py
@@ -15,14 +15,24 @@ from pyrevit import forms
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Function to display select room number
-def roomNumber():
+# Rooms wrapper
+class RoomsForm(forms.TemplateListItem):
+	@ property
+	def name(self):
+		return '{} --- {}'.format(self.item.Number, self.item.LookupParameter("Name").AsString())
+
+# Function to display select room
+def roomElev():
 	# Collect all rooms
 	rooms = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Rooms)
 	# Create set of rooms
-	roomNumber = [r.LookupParameter("Number").AsString() + "    ---    " + r.LookupParameter("Name").AsString() for r in rooms]
+	roomNumber = sorted([RoomsForm(x) for x in rooms], key=lambda x: x.Number)
 	# Prompt selection list
-	return forms.SelectFromList.show(roomNumber, "Rooms to elevate", 600, 600, multiselect = True)
+	return forms.SelectFromList.show(roomNumber, "Rooms to Elevate", 600, 600, multiselect = True)
+
+def roomCenter(room):
+	per = room.Perimeter
+	return per
 
 # Prompt to select rooms
-roomNumber()
+roomsElev = roomElev()
