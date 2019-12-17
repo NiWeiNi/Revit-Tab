6683e8ad8d8569c1ffffdb6fee8aa51212730f12
NiweiNi <wnini@hotmail.es>
2019-12-18 09:52:37 +1100

    refactor: refactor to functions

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Selection.Panel/Isolate Category.pushbutton/Isolate_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Selection.Panel/Isolate Category.pushbutton/Isolate_script.py
@@ -1,67 +1,76 @@
 # -*- coding: utf-8 -*-
-"""Isolates selected categories and overriden the rest of them with 90% transparency.
+"""Isolates selected categories and overriden the rest of them with 50% transparency.
 """
 __title__ = 'Isolate\nCategory'
 __author__ = "nWn"
 
-# Import commom language runtime
-import clr
+# Import 
+from pyrevit import revit, DB, UI
+from pyrevit import script
+from pyrevit import forms
 
-# Import Revit UI
-from Autodesk.Revit.UI.Selection import ObjectType
-from Autodesk.Revit.UI import TaskDialog
-
-# Import Revit DB
-from Autodesk.Revit.DB import OverrideGraphicSettings, Transaction
-
-# Store current document to variable
+# Store current document into variable
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Retrieve current view
-activeView = doc.ActiveView
+# Function to retrieve all categories in the document
+def cat():
+	docCat = doc.Settings.Categories
+	return docCat
 
-# Retrieve all categories in the document
-docCat = doc.Settings.Categories
+# Function to pick elements
+def pickEl():
+	# Show task dialog to inform user
+	UI.TaskDialog.Show("Select elements", "Select elements of categories to isolate")
+	# Select elements
+	objType = UI.Selection.ObjectType.Element
+	selection = uidoc.Selection.PickObjects(objType, "Pick elements to isolate with transparency")
+	return selection
 
-# Show task dialog to inform user
-TaskDialog.Show("Select elements", "Select elements from categories to isolate")
+# Function to retrieve categories from selection
+def retCat(selection):
+	# Store categories of the selected elements
+	categories = []
+	for e in selection:
+		elem = doc.GetElement(e.ElementId)
+		cat = doc.GetElement(e.ElementId).Category.Name
+		# Check if element is group or assembly to extract elements
+		if cat == "Assemblies" or cat == "Model Groups":
+			ids = elem.GetMemberIds()
+			# Get elements category inside groups or assemblies
+			for id in ids:
+				catsub = doc.GetElement(id).Category.Name
+				categories.append(catsub)
+		else:
+			categories.append(cat)
+	return categories
 
-# Select elements
-objType = ObjectType.Element
-selection = uidoc.Selection.PickObjects(objType, "Pick elements to isolate with transparency")
+# Function to override transparency
+def overCat(categories, docCat):
+	# Retrieve current view
+	activeView = doc.ActiveView
+	# Override tranparency settings
+	overtransparency = DB.OverrideGraphicSettings()
+	overtransparency.SetSurfaceTransparency(50)
 
-# Store categories of the selected elements
-categories = []
-for e in selection:
-	elem = doc.GetElement(e.ElementId)
-	cat = doc.GetElement(e.ElementId).Category.Name
-	# Check if element is group or assembly to extract elements
-	if cat == "Assemblies" or cat == "Model Groups":
-		ids = elem.GetMemberIds()
-		# Get elements category inside groups or assemblies
-		for id in ids:
-			catsub = doc.GetElement(id).Category.Name
-			categories.append(catsub)
-	else:
-		categories.append(cat)
+	# Create a individual transaction
+	t = DB.Transaction(doc, "Override category visibility")
+	# Start individual transaction
+	t.Start()
+	# Override all categories transparency
+	for c in docCat:
+		if c.Name not in categories:
+			try:
+				activeView.SetCategoryOverrides(c.Id, overtransparency)
+			except:
+				pass
+	# Commit transaction
+	t.Commit()
 
-# Override tranparency settings
-overtransparency = OverrideGraphicSettings()
-overtransparency.SetSurfaceTransparency(50)
+# Function to override categories
+def overrideCat():
+	sel = pickEl()
+	cats = retCat(sel)
+	overCat(cats, cat())
 
-# Create a individual transaction
-t = Transaction(doc, "Override category visibility")
-# Start individual transaction
-t.Start()
-
-# Override all categories transparency
-for c in docCat:
-	if c.Name not in categories:
-		try:
-			activeView.SetCategoryOverrides(c.Id, overtransparency)
-		except:
-			pass
-
-# Commit transaction
-t.Commit()
+overrideCat()
