d1033f4e30d70473afa78aee78dfa2390d644c68
Wei <weinini007@gmail.com>
2020-05-13 12:47:53 +1000

    refactor: set final destination for pdfs

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Print by Revision.pushbutton/Print by Revision_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Print by Revision.pushbutton/Print by Revision_script.py
@@ -9,11 +9,12 @@ __author__ = "nWn"
 
 # Import commom language runtime
 import clr
+# Import winreg to edit Registry
+import _winreg
 
 # Import Revit DB
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, \
-                            Transaction, TransactionGroup, ElementId, PrintManager, \
-							PrintSetting, ViewSet
+from pyrevit import forms
+from pyrevit import DB
 
 # Import libraries to enable Windows forms
 clr.AddReference('System.Windows.Forms')
@@ -22,11 +23,6 @@ clr.AddReference('System.Drawing')
 from System.Drawing import Point, Size
 from System.Windows.Forms import Application, Button, Form, Label, TextBox, FolderBrowserDialog, DialogResult
 
-# Import os, shutil to move files
-import os, shutil
-
-# Import sleep for moving files after printing
-from time import sleep
 
 # Create a class form
 class CreateWindow(Form):
@@ -48,7 +44,7 @@ class CreateWindow(Form):
 		# Create TextBox for input
 		self.textboxDiv = TextBox()
 		self.textboxDiv.Parent = self
-		self.textboxDiv.Text = "01/22/2019"
+		self.textboxDiv.Text = "Input Revision Date"
 		self.textboxDiv.Location = Point(300, 20)
 	
 		# Create button
@@ -69,62 +65,34 @@ class CreateWindow(Form):
 			except:
 				self.Close()
 
-# Path to save the pdfs by default
-directory = "C:\Users\Snoopy\Desktop"
+
+# Function to set the printing path in the registry
+def changePrintPath(printPath):
+	try:
+		key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, keyValue, 0, _winreg.KEY_ALL_ACCESS)
+		originalKeyValue = _winreg.QueryValueEx(key, "AutoSaveDir")
+	except:
+		pass
+
+	_winreg.SetValueEx(key, "AutoSaveDir", 0, _winreg.REG_SZ, printPath)
+	_winreg.CloseKey(key)
+
+# Function to return the the registry to original state
+def unmodifyRegistry(keyValue, originalKeyValue):
+	try:
+		key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, keyValue, 0, _winreg.KEY_ALL_ACCESS)
+		_winreg.SetValueEx(key, "AutoSaveDir", 0, _winreg.REG_SZ, originalKeyValue)
+		_winreg.CloseKey(key)
+	except:
+		pass
 
 # Function to create Windows folder browser
 def windBrowser():
-    # Create folder browser window 
-    dialog = FolderBrowserDialog()
-    # Record for user action and store selected path
-    if (dialog.ShowDialog() == DialogResult.OK):
-        return dialog.SelectedPath
-
-# Call windBrowser to select folder with backup files and store path to var
-finalDirectory = windBrowser()
-
-# Call the CreateWindow class and create the input for Revision Date
-formRevision = CreateWindow("Revision Date on Sheets to Print", "Input Revision Date")
-Application.Run(formRevision)
-
-# Assign the input to variable
-revDate = formRevision.value
-
-# Store current document to variable
-doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
-
-# Collects all sheets in current document
-sheetsCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets)
-
-# Variable to store sheets with revisions
-sheets = []
-
-# Get curent revision on sheets and collect sheets
-for s in sheetsCollector:
-	if s.GetCurrentRevision() != ElementId.InvalidElementId and doc.GetElement(s.GetCurrentRevision()).RevisionDate == revDate:
-		rev = doc.GetElement(s.GetCurrentRevision())
-		sheets.append(s)
-
-# Retrieve sheets number, name and revision
-outName = []
-finalName = []
-for s in sheets:
-	number = s.SheetNumber
-	name = s.Name
-	revision = s.GetRevisionNumberOnSheet(doc.GetElement(s.GetCurrentRevision()).Id)
-	pdfName = directory + "\\" + number + " - " + name + "[" + revision + "]" + ".pdf"
-	finalPdfName = finalDirectory + "\\" + number + " - " + name + "[" + revision + "]" + ".pdf"
-	outName.append(pdfName)
-	finalName.append(finalPdfName)
-
-# Check if there are files with same name and clean them
-for file in outName:
-	if os.path.exists(file):
-		os.remove(file)
-
-# Collect all print settings from document
-printSettingCollector = FilteredElementCollector(doc).OfClass(PrintSetting)
+	# Create folder browser window 
+	dialog = FolderBrowserDialog()
+	# Record for user action and store selected path
+	if (dialog.ShowDialog() == DialogResult.OK):
+		return dialog.SelectedPath
 
 # Function to pick printSetting by Name
 def pickPrintSetting(name):
@@ -141,7 +109,7 @@ def printSheet(sheet, printerName, combined, filePath, printSettingName):
 	printManager.Apply()
 
 	# Define current view set as current
-	viewSet = ViewSet()
+	viewSet = DB.ViewSet()
 	viewSet.Insert(sheet)
 	viewSheetSetting = printManager.ViewSheetSetting
 	viewSheetSetting.CurrentViewSheetSet.Views = viewSet
@@ -151,10 +119,13 @@ def printSheet(sheet, printerName, combined, filePath, printSettingName):
 	printManager.SelectNewPrintDriver(printerName)
 	printManager.Apply()
 
-	# Print to file
+	# Check if printer is virtual and set print to file
 	printManager.CombinedFile = combined
 	printManager.Apply()
-	printManager.PrintToFile = True
+	if printManager.IsVirtual:
+		printManager.PrintToFile = True
+	else:
+		printManager.PrintToFile = False
 	printManager.Apply()
 
 	# Set destination filepath
@@ -172,41 +143,81 @@ def printSheet(sheet, printerName, combined, filePath, printSettingName):
 	# Delete Current viewSheetSettings to allow new setting to be stored
 	viewSheetSetting.Delete()
 
-# Create a individual transaction
-t = Transaction(doc, "Batch Print")
-# Start transaction
-t.Start()
+# Function to display results to user
+def printMessage(resultList, message, messageWarning):
+	if len(resultList) != 0:
+		print(message)
+		print("\n".join(resultList))
+	else:
+		print(messageWarning)
 
-# Print sheets
+""" Start of printing script """
+# Global values, predefined parameters
+originalKeyValue = ""
+keyValue = r"Software\PDFPrint\Services\PDF"
+# Variable to store sheets with revisions
+sheets = []
+# Naming of pdf files
+fileNames = []
+# Printed and failed sheets
 printedSheets = []
 failedSheets = []
-for sheet, fileName in zip(sheets, outName):
+# Store current document to variable
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+
+# Call windBrowser to select folder
+printFolder = windBrowser()
+
+# Call the CreateWindow class and create the input for Revision Date
+formRevision = CreateWindow("Revision Date on Sheets to Print", "Input Revision Date")
+Application.Run(formRevision)
+
+# Assign the input to variable
+revDate = formRevision.value
+
+# Collects all sheets in current document
+sheetsCollector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Sheets)
+
+# Get curent revision on sheets and collect sheets
+for s in sheetsCollector:
+	"""
+	if s.GetCurrentRevision() != DB.ElementId.InvalidElementId and \
+		doc.GetElement(s.GetCurrentRevision()).RevisionDate == revDate:
+		rev = doc.GetElement(s.GetCurrentRevision())
+		"""
+	sheets.append(s)
+
+# Retrieve sheets number, name and revision
+for s in sheets:
+	number = s.SheetNumber
+	name = s.Name
+	revision = "Alo"
+	# revision = s.GetRevisionNumberOnSheet(doc.GetElement(s.GetCurrentRevision()).Id)
+	pdfName = printFolder + "\\" + number + " - " + name + "[" + revision + "]" + ".pdf"
+	fileNames.append(pdfName)
+
+# Collect all print settings from document
+printSettingCollector = DB.FilteredElementCollector(doc).OfClass(DB.PrintSetting)
+
+# Change path to save printed files
+changePrintPath(printFolder)
+
+# Create a individual transaction
+t = DB.Transaction(doc, "Batch Print")
+# Start transaction
+t.Start()
+for sheet, fileName in zip(sheets, fileNames):
 	try:
 		printSheet(sheet, "PDF24", True, fileName, "A3")
 		printedSheets.append(sheet.SheetNumber)
 	except:
 		failedSheets.append(fileName)
-
 # Commit transaction
 t.Commit()
 
-# Function to Move Files
-def moveFiles(origin, destination):
-	for fileName, finalDestination in zip(outName, finalName):
-		shutil.move(fileName, finalDestination)
-
-# Set timer to call function and Move files to final destination
-timeToPrint = 4
-sleep(len(outName) * timeToPrint)
-moveFiles(outName, finalName)
-
-# Function to display result to user
-def printMessage(resultList, message, messageWarning):
-    if len(resultList) != 0:
-        print(message)
-        print("\n".join(resultList))
-    else:
-        print(messageWarning)
+# Return Registry to original path
+unmodifyRegistry(keyValue, originalKeyValue)
 
 # Print message
 printMessage(printedSheets, "The following sheets have been printed:", "No file has been printed.") 
