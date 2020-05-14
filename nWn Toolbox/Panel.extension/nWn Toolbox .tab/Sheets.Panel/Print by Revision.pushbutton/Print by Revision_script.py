b54c7dd334b504e682519981983b3f258b975b15
Wei <weinini007@gmail.com>
2020-05-14 10:50:44 +1000

    fix: last pdf won't print in default dir

--- nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Print by Revision.pushbutton/Print by Revision_script.py
+++ nWn Toolbox/Panel.extension/nWn Toolbox .tab/Sheets.Panel/Print by Revision.pushbutton/Print by Revision_script.py
@@ -23,6 +23,22 @@ clr.AddReference('System.Drawing')
 from System.Drawing import Point, Size
 from System.Windows.Forms import Application, Button, Form, Label, TextBox, FolderBrowserDialog, DialogResult
 
+# Global values, predefined parameters
+originalKeyValue = ""
+keyValue = r"Software\PDFPrint\Services\PDF"
+# Variable to store sheets with revisions
+sheets = []
+# Naming of pdf files
+fileNames = []
+# Print configuration
+selectedPrinter = "PDF24"
+pageConfig = "A3"
+# Printed and failed sheets
+printedSheets = []
+failedSheets = []
+# Store current document to variable
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
 
 # Create a class form
 class CreateWindow(Form):
@@ -38,13 +54,14 @@ class CreateWindow(Form):
 		# Create label for input title
 		labelDiv = Label(Text = author + ":")
 		labelDiv.Parent = self
-		labelDiv.Size = Size(100, 150)
+		labelDiv.Size = Size(250, 250)
 		labelDiv.Location = Point(30, 20)
 		
 		# Create TextBox for input
 		self.textboxDiv = TextBox()
 		self.textboxDiv.Parent = self
-		self.textboxDiv.Text = "Input Revision Date"
+		self.textboxDiv.Text = "Date 1"
+		self.textboxDiv.Size = Size(150, 150)
 		self.textboxDiv.Location = Point(300, 20)
 	
 		# Create button
@@ -65,20 +82,19 @@ class CreateWindow(Form):
 			except:
 				self.Close()
 
-
 # Function to set the printing path in the registry
 def changePrintPath(printPath):
 	try:
+		global originalKeyValue
 		key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, keyValue, 0, _winreg.KEY_ALL_ACCESS)
-		originalKeyValue = _winreg.QueryValueEx(key, "AutoSaveDir")
+		originalKeyValue, regType = _winreg.QueryValueEx(key, "AutoSaveDir")
+		_winreg.SetValueEx(key, "AutoSaveDir", 0, _winreg.REG_SZ, printPath)
+		_winreg.CloseKey(key)
 	except:
 		pass
 
-	_winreg.SetValueEx(key, "AutoSaveDir", 0, _winreg.REG_SZ, printPath)
-	_winreg.CloseKey(key)
-
 # Function to return the the registry to original state
-def unmodifyRegistry(keyValue, originalKeyValue):
+def unmodifyRegistry(originalKeyValue):
 	try:
 		key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, keyValue, 0, _winreg.KEY_ALL_ACCESS)
 		_winreg.SetValueEx(key, "AutoSaveDir", 0, _winreg.REG_SZ, originalKeyValue)
@@ -101,47 +117,53 @@ def pickPrintSetting(name):
 			return p
 
 # Function to print
-def printSheet(sheet, printerName, combined, filePath, printSettingName):
+def printSheet(sheets, printerName, combined, fileNames, printSettingName):
+	for sheet, filePath in zip(sheets, fileNames):
+		# Set print range
+		printManager = doc.PrintManager
+		printManager.PrintRange = printManager.PrintRange.Select
+		printManager.Apply()
 
-	# Set print range
-	printManager = doc.PrintManager
-	printManager.PrintRange = printManager.PrintRange.Select
-	printManager.Apply()
+		# Define current view set as current
+		viewSet = DB.ViewSet()
+		viewSet.Insert(sheet)
+		viewSheetSetting = printManager.ViewSheetSetting
+		viewSheetSetting.CurrentViewSheetSet.Views = viewSet
+		viewSheetSetting.SaveAs("Current Print")
 
-	# Define current view set as current
-	viewSet = DB.ViewSet()
-	viewSet.Insert(sheet)
-	viewSheetSetting = printManager.ViewSheetSetting
-	viewSheetSetting.CurrentViewSheetSet.Views = viewSet
-	viewSheetSetting.SaveAs("Current Print")
+		# Set printer
+		printManager.SelectNewPrintDriver(printerName)
+		printManager.Apply()
 
-	# Set printer
-	printManager.SelectNewPrintDriver(printerName)
-	printManager.Apply()
+		# Check if printer is virtual and set print to file
+		printManager.CombinedFile = combined
+		printManager.Apply()
+		if printManager.IsVirtual:
+			printManager.PrintToFile = True
+		else:
+			printManager.PrintToFile = False
+		printManager.Apply()
 
-	# Check if printer is virtual and set print to file
-	printManager.CombinedFile = combined
-	printManager.Apply()
-	if printManager.IsVirtual:
-		printManager.PrintToFile = True
-	else:
-		printManager.PrintToFile = False
-	printManager.Apply()
+		# Set destination filepath
+		printManager.PrintToFileName = filePath
+		printManager.Apply()
 
-	# Set destination filepath
-	printManager.PrintToFileName = filePath
-	printManager.Apply()
+		# Set print setting
+		printSetup = printManager.PrintSetup
+		printSetup.CurrentPrintSetting = pickPrintSetting(printSettingName)
+		printManager.Apply()
 
-	# Set print setting
-	printSetup = printManager.PrintSetup
-	printSetup.CurrentPrintSetting = pickPrintSetting(printSettingName)
-	printManager.Apply()
+		try:
+			# Submit to printer
+			printManager.SubmitPrint()
+			# Append sheet as successful 
+			printedSheets.append(sheet.SheetNumber)
+		except:
+			# Append sheet as failed
+			failedSheets.append(filePath)
 
-	# Submit to printer
-	printManager.SubmitPrint()
-
-	# Delete Current viewSheetSettings to allow new setting to be stored
-	viewSheetSetting.Delete()
+		# Delete Current viewSheetSettings to allow new setting to be stored
+		viewSheetSetting.Delete()
 
 # Function to display results to user
 def printMessage(resultList, message, messageWarning):
@@ -150,21 +172,6 @@ def printMessage(resultList, message, messageWarning):
 		print("\n".join(resultList))
 	else:
 		print(messageWarning)
-
-""" Start of printing script """
-# Global values, predefined parameters
-originalKeyValue = ""
-keyValue = r"Software\PDFPrint\Services\PDF"
-# Variable to store sheets with revisions
-sheets = []
-# Naming of pdf files
-fileNames = []
-# Printed and failed sheets
-printedSheets = []
-failedSheets = []
-# Store current document to variable
-doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
 
 # Call windBrowser to select folder
 printFolder = windBrowser()
@@ -181,19 +188,16 @@ sheetsCollector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory
 
 # Get curent revision on sheets and collect sheets
 for s in sheetsCollector:
-	"""
 	if s.GetCurrentRevision() != DB.ElementId.InvalidElementId and \
 		doc.GetElement(s.GetCurrentRevision()).RevisionDate == revDate:
 		rev = doc.GetElement(s.GetCurrentRevision())
-		"""
-	sheets.append(s)
+		sheets.append(s)
 
 # Retrieve sheets number, name and revision
 for s in sheets:
 	number = s.SheetNumber
 	name = s.Name
-	revision = "Alo"
-	# revision = s.GetRevisionNumberOnSheet(doc.GetElement(s.GetCurrentRevision()).Id)
+	revision = s.GetRevisionNumberOnSheet(doc.GetElement(s.GetCurrentRevision()).Id)
 	pdfName = printFolder + "\\" + number + " - " + name + "[" + revision + "]" + ".pdf"
 	fileNames.append(pdfName)
 
@@ -207,17 +211,13 @@ changePrintPath(printFolder)
 t = DB.Transaction(doc, "Batch Print")
 # Start transaction
 t.Start()
-for sheet, fileName in zip(sheets, fileNames):
-	try:
-		printSheet(sheet, "PDF24", True, fileName, "A3")
-		printedSheets.append(sheet.SheetNumber)
-	except:
-		failedSheets.append(fileName)
+printSheet(sheets, selectedPrinter, True, fileNames, pageConfig)
+
 # Commit transaction
 t.Commit()
 
-# Return Registry to original path
-unmodifyRegistry(keyValue, originalKeyValue)
-
 # Print message
 printMessage(printedSheets, "The following sheets have been printed:", "No file has been printed.") 
+
+# Return Registry to original path
+unmodifyRegistry(originalKeyValue)
